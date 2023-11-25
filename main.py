9467265a0f1a92205a8ade3b8e0a8b6fa8b89d16
Shiqi-Xia <3174411405@qq.com>
2023-11-25 14:30:24 -0800

    Add DDQN

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ episode_durations = []
 
 
 
-def optimize_model():
+def DQN_optimize_model():
     if len(memory) < BATCH_SIZE:
         return
     transitions = memory.sample(BATCH_SIZE)
@@ -127,6 +127,52 @@ def optimize_model():
     # In-place gradient clipping
     torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
     optimizer.step()
+
+
+
+def DDQN_optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+
+    batch = Transition(*zip(*transitions))
+    # Compute a mask of non-final states and concatenate the batch elements
+    # (a final state would've been the one after which simulation ended)
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                            batch.next_state)), device=device, dtype=torch.bool)
+    if batch.next_state is not None:
+        non_final_next_states = torch.cat([s for s in batch.next_state
+                                           if s is not None])
+    # print("batch.state:",batch.state)
+    state_batch = torch.cat(batch.state)
+    # print("state_batch:",state_batch.shape)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+    # columns of actions taken. These are the actions which would've been taken
+    # for each batch state according to policy_net
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+    # Compute V(s_{t+1}) for all next states.
+    # Expected values of actions for non_final_next_states are computed based
+    # on the "older" target_net; selecting their best reward with max(1)[0].
+    # This is merged based on the mask, such that we'll have either the expected
+    # state value or 0 in case the state was final.
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+    with torch.no_grad():
+        max_action = policy_net(non_final_next_states).max(1)[1].view(-1, 1)
+        next_state_values[non_final_mask] = torch.squeeze(target_net(non_final_next_states).gather(1, max_action))
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+    # Compute Huber loss
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+    # In-place gradient clipping
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optimizer.step()
+
 
 
 if torch.cuda.is_available():
@@ -178,7 +224,8 @@ for i_episode in range(num_episodes):
         stacked_next_state = torch.stack(frames).unsqueeze(0)
         memory.push(Transition, stacked_state, action, stacked_next_state, reward_tensor)
         stacked_state = stacked_next_state
-        optimize_model()
+        # DQN_optimize_model()
+        DDQN_optimize_model()
         target_net_state_dict = target_net.state_dict()
         policy_net_state_dict = policy_net.state_dict()
         for key in policy_net_state_dict:
